ff7170cfce521379260b889ef5089ed7c9473886
Pedro Henrique Bastos <pedrobastos83@gmail.com>
2017-09-23 16:02:29 -0300

    atualização dos códigos
    códigos corrigidos e reduzidos, agora funcionam perfeitamente

--- mne_simpson.m
+++ mne_simpson.m
@@ -1,11 +1,7 @@
 function v = mne_simpson(a,b,n,nome_da_funcao)
   hrange = (b-a)/(2*n); %calculando o valor de h
-  for count = 0:1:(2*n) %pegando os valores de f(xi)
-    aux = a + hrange*count;
-    results(count) = nome_da_funcao(aux);
-  end
   sum = 0;
-  for count = 1:2:n-1 %fazendo a soma dos f(xi) intermediarios
-    sum = sum + 2*nome_da_funcao(results(count)) + nome_da_funcao(results(count); 
+  for count = 1:2:(2*n)-1 %pegando e somando os valores dos f(xi intermediarios
+    sum = sum + 2*(nome_da_funcao(a + hrange*(count))) + nome_da_funcao(a+ hrange*(count+1));
   end
-  v = (hrange/3)*((2*soma)+nome_da_funcao(results(0))-nome_da_funcao(results(n))); %ajustando para somar f(x0) e f(xn)
+  v = (hrange/3)*((2*sum)+nome_da_funcao(a)-nome_da_funcao(b)); %ajustando para somar f(x0) e f(xn)
--- mne_trapezio.m
+++ mne_trapezio.m
@@ -1,11 +1,8 @@
 function i = mne_trapezio(a,b,n,nome_da_funcao)
-  hrange = (b-a)/n %calculando o valor de h
-  for count = 0:1:n %pegando os valores de f(xi)
-    aux = a + hrange*count;
-    results(count) = nome_da_funcao(aux);
-  end
+  hrange = (b-a)/n; %calculando o valor de h
   sum = 0;
-  for count = 1:1:n-1 %fazendo a soma dos f(xi) intermediarios
-    sum = sum + nome_da_funcao(results(count));
+  for count = 2:1:n %pegando os valores intermediários de xi, e somando os respectivos f(xi)
+    aux = a + hrange*(count-1);
+    sum = sum + nome_da_funcao(aux);
   end
-  i = (h/2)*((2*sum) + nome_da_funcao(results(0)) + nome_da_funcao(results(0))); %ajustando para somar f(x0) e f(xn)
+  i = (hrange/2)*((2*sum) + nome_da_funcao(a) + nome_da_funcao(b)); %ajustando para somar f(x0) e f(xn)
