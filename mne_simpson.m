b8c58ded0626a7a68d7b47a089354a5a013751eb
BastosPedro <pedrobastos83@gmail.com>
2018-04-20 21:03:26 -0300

    adaptado
    adaptado para o problema; uso direto de feval para melhor versatilidade

--- mne_simpson.m
+++ mne_simpson.m
@@ -2,6 +2,6 @@ function v = mne_simpson(a,b,n,nome_da_funcao)
   hrange = (b-a)/(2*n); %calculando o valor de h
   sum = 0;
   for count = 1:2:(2*n)-1 %pegando e somando os valores dos f(xi intermediarios
-    sum = sum + 2*(nome_da_funcao(a + hrange*(count))) + nome_da_funcao(a+ hrange*(count+1));
+    sum = sum + 2*(feval(nome_da_funcao,a + hrange*(count))) + feval(nome_da_funcao, a+ hrange*(count+1));
   end
-  v = (hrange/3)*((2*sum)+nome_da_funcao(a)-nome_da_funcao(b)); %ajustando para somar f(x0) e f(xn)
+  v = (hrange/3)*((2*sum)+feval(nome_da_funcao,a) - feval(nome_da_funcao,b)); %ajustando para somar f(x0) e f(xn)
--- mne_trapezio.m
+++ mne_trapezio.m
@@ -1,8 +1,8 @@
 function i = mne_trapezio(a,b,n,nome_da_funcao)
   hrange = (b-a)/n; %calculando o valor de h
   sum = 0;
-  for count = 2:1:n %pegando os valores intermediários de xi, e somando os respectivos f(xi)
+  for count = 2:1:n %pegando os valores intermediarios de xi, e somando os respectivos f(xi)
     aux = a + hrange*(count-1);
-    sum = sum + nome_da_funcao(aux);
+    sum = sum + feval(nome_da_funcao, aux);
   end
-  i = (hrange/2)*((2*sum) + nome_da_funcao(a) + nome_da_funcao(b)); %ajustando para somar f(x0) e f(xn)
+  i = (hrange/2)*((2*sum) + feval(nome_da_funcao, a) + feval(nome_da_funcao, b)); %ajustando para somar f(x0) e f(xn)
